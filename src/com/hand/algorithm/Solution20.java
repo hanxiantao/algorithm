4f5e6f7ae6724b47290a1351d6e13647f13ed29f
韩贤涛 <601803023@qq.com>
2020-01-06 22:36:54 +0800

    2020.01.06：17-20题

---
+++ src/com/hand/algorithm/Solution20.java
@@ -0,0 +1,34 @@
+package com.hand.algorithm;
+
+import java.util.Stack;
+
+/**
+ * 包含min函数的栈：
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数（时间复杂度应为O（1））
+ */
+public class Solution20 {
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
+
+    public void push(int node) {
+        stack.push(node);
+        if (minStack.empty() || node <= minStack.peek()) {
+            minStack.push(node);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
